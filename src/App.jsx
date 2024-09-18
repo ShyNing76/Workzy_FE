8e61869e8fce28eb8dd5fff101d0d91bd2945d40
ShyNing76 <doduylong070604@gmail.com>
2024-09-18 15:52:18 +0700

    Update route and page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import Navbar from "./components/layout/Navbar.jsx";
+import {Outlet} from "react-router-dom";
 
 function App() {
 
   return <>
-    
+    <Navbar/>
+    <Outlet/>
   </>
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,37 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/index.css"
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import RegisterPage from "./pages/RegisterPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                index: true,
+                element: <HomePage />,
+            },
+
+
+        ],
+    },
+    {
+        path: "register",
+        element: <RegisterPage />,
+    },
+    {
+        path: "login",
+        element: <LoginPage />,
+    },
+]);
+
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
-  <App />
+    <RouterProvider router={router} />
   // </StrictMode>,
 );
